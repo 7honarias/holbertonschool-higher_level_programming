7b2ca1a0fc9c96842b0b03b6439588e7ba212989
Jhon Arias <7honarias@gmail.com>
2020-12-09 01:09:54 +0000

    [UPDATE] - error with function chr

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,8 @@
 def uppercase(str):
     for i in str:
         if (ord(i) >= 97) and (ord(i) <= 122):
-            print('{:c}'.format(ord(i) - 32), end = '')
+            i = chr(ord(i) - 32)
+            print('{}'.format(i), end = '')
         else:
-            print('{:c}'.format(ord(i)), end = '')
+            print('{}'.format(i), end = '')
     print()

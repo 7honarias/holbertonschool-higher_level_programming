aa926534f202b139260333a83417ce9e93c7b2d9
Jhon Arias <7honarias@gmail.com>
2020-12-10 18:13:25 +0000

    [ADD] - Exercise of argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    args = argv
+    if (len(args) - 1) == 1:
+        print("{} argument:".format(len(args) - 1))
+    else:
+        print("{} arguments:".format(len(args) - 1))
+
+        for i in args:
+            if args.index(i) == 0:
+                continue
+            print("{}: {}".format(args.index(i), i))

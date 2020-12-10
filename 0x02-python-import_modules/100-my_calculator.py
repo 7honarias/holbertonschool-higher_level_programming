39a17ce912bee6c28bc11c88ed2bbc66ab809a1d
Jhon Arias <7honarias@gmail.com>
2020-12-10 18:53:56 +0000

    [UPDATE] - error if __name__

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 from sys import argv
-if __name__ == "__main__"
-arguments = argv
-size = len(arguments)
-if size != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+if __name__ == "__main__":
+    arguments = argv
+    size = len(arguments)
+    if size != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-operator = arguments[2]
-num1 = int(arguments[1])
-num2 = int(arguments[3])
+    operator = arguments[2]
+    num1 = int(arguments[1])
+    num2 = int(arguments[3])
 
-if operator == "+":
-    print("{} + {} = {}".format(num1, num2, num1 + num2))
-elif operator == "-":
-    print("{} - {} = {}".format(num1, num2, num1 - num2))
-elif operator == "*":
-    print("{} * {} = {}".format(num1, num2, num1 * num2))
-elif operator == "/":
-    print("{} / {} = {}".format(num1, num2, num1 / num2))
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
+    if operator == "+":
+        print("{} + {} = {}".format(num1, num2, num1 + num2))
+    elif operator == "-":
+        print("{} - {} = {}".format(num1, num2, num1 - num2))
+    elif operator == "*":
+        print("{} * {} = {}".format(num1, num2, num1 * num2))
+    elif operator == "/":
+        print("{} / {} = {}".format(num1, num2, num1 / num2))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)

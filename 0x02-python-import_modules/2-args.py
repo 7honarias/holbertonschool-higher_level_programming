079c4521970401a898acae01d95f44a1c6c4e1b0
Jhon Arias <7honarias@gmail.com>
2020-12-10 18:24:30 +0000

    [UPDATE] - Correction in format

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,13 @@ from sys import argv
 if __name__ == "__main__":
     args = argv
     if (len(args) - 1) == 1:
-        print("{} argument:".format(len(args) - 1))
+        print("{:d} argument:".format(len(args) - 1))
     elif (len(args) - 1) == 0:
-        print("{} arguments.".format(len(args) - 1))
+        print("{:d} arguments.".format(len(args) - 1))
     else:
-        print("{} arguments:".format(len(args) - 1))
+        print("{:d} arguments:".format(len(args) - 1))
 
         for i in args:
             if args.index(i) == 0:
                 continue
-            print("{}: {}".format(args.index(i), i))
+            print("{:d}: {:s}".format(args.index(i), i))

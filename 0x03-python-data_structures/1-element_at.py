033a6e63f9c71c5859b19f075f7e739cf32ed7c2
Jhon Arias <7honarias@gmail.com>
2020-12-15 17:30:49 +0000

    [ADD] - funciton replace element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
 def element_at(my_list, idx):
+    """Function that add a element
+    Args:
+        my_list: list to print
+        idx: index to add
+    Returns:
+        my_list modific or None
+    """
     if idx < 0 or len(my_list) < idx:
         return (None)
 
---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    """Function that replace a element
+    Args:
+        my_list: list to print
+    Returns:
+        my_list always
+    """
+    if (idx < 0) or (len(my_list) < idx):
+        return (my_list)
+
+    my_list[idx] = element
+    return (my_list)

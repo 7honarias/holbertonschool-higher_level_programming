cac63c63ef4d7eb71eaf0573e0462ee376775cf0
Jhon Arias <7honarias@gmail.com>
2020-12-15 18:07:47 +0000

    [UPDATE] - use functiion copy()

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -10,7 +10,6 @@ def new_in_list(my_list, idx, element):
     """
     if idx < 0 or len(my_list) <= 0:
         return (my_list)
-
-    new_list = my_list[:]
+    new_list = my_list.copy()
     new_list[idx] = element
     return (new_list)

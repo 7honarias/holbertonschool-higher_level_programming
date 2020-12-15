af13023b4cdce960e507bfe3175d06cabcd9cad6
Jhon Arias <7honarias@gmail.com>
2020-12-15 17:26:15 +0000

    [ADD] - funciton add element

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """Function that prints a list
+    Args:
+        my_list: list to print
+    Returns:
+        Always nothing.
+    """
     for i in my_list:
-        print("{}".format(i))
+        print("{:d}".format(i))

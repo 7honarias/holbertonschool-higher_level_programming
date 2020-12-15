89efa164a1a46ca39eaa783bafd0b46e4207570a
Jhon Arias <7honarias@gmail.com>
2020-12-15 18:02:04 +0000

    [UPDATE] - Correction comment an if my list is FALSE

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -7,7 +7,7 @@ def print_reversed_list_integer(my_list=[]):
         Always nothing.
     """
     len_list = len(my_list) - 1
-
-    while len_list >= 0:
-        print("{:d}".format(my_list[len_list]))
-        len_list -= 1
+    if my_list:
+        while len_list >= 0:
+            print("{:d}".format(my_list[len_list]))
+            len_list -= 1
---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """Function that prints a list
+    Args:
+        my_list: list to print
+        idx: index
+        element: number to change
+    Returns:
+        new list
+    """
+    if idx < 0 or len(my_list) <= 0:
+        return (my_list)
+
+    new_list = my_list[:]
+    new_list[idx] = element
+    return (new_list)

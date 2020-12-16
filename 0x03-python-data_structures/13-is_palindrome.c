6261fb8a9a5b1a32bf6c0d56969b93adf6f6f0b8
Jhon Arias <7honarias@gmail.com>
2020-12-16 03:09:51 +0000

    [UPDATE] - new function add_nodeint

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,28 +9,49 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *temp = *head;
+	listint_t *temp_last = NULL;
 	listint_t *last = NULL;
 	int x;
-	int count = 0;
 
 	if (head && *head)
 	{
 		while (temp)
 		{
 			x = temp->n;
-			add_nodeint_end(&last, x);
-			count++;
+			add_nodeint(&last, x);
 			temp = temp->next;
 		}
 		temp = *head;
+		temp_last = last;
 		while (temp)
 		{
-			if (temp->n != last->n)
+			if (temp->n != temp_last->n)
 				return (0);
 			temp = temp->next;
-			last = last->next;
+			temp_last = temp_last->next;
 		}
 	}
 	free_listint(last);
 	return (1);
+}
+
+/**
+ * add_nodeint - add node to list
+ * @head: head of list
+ * @n: number to add
+ * Return: pointer new head
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	(*head) = new;
+
+	return (new);
 }

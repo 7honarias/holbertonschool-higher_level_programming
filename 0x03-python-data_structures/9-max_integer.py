3c15aff3f5d5b483e01c95925599b925fa582849
Jhon Arias <7honarias@gmail.com>
2020-12-16 00:39:49 +0000

    [ADD] - correction for sort function

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    maxi = 0
-    if len(my_list) == 0 and not(my_list):
-        return (None)
 
-    for i in my_list:
-        if i > maxi:
-            maxi = i
+    if my_list:
+        my_list.sort()
+        return (my_list[-1])
 
-    return (maxi)
+    return (None)

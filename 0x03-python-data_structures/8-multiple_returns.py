ab90e007a2c86dde26467b32d042e0b67b60d1b6
Jhon Arias <7honarias@gmail.com>
2020-12-16 00:10:00 +0000

    [UPDATE] - correcion in None,

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0 and sentence:
-        return (none,)
+    if len(sentence) == 0:
+        return (None,)
     return(len(sentence), sentence[0])

de85a935544329e4da4ba78e7102d2ec40c1d181
Jhon Arias <7honarias@gmail.com>
2020-12-17 17:58:24 +0000

    [ADD] - key with the biggest integer

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return (None)
+
+    x = 0;
+    for element in a_dictionary:
+        if a_dictionary[element] > x:
+            x = a_dictionary[element]
+            ids = element
+    return (ids)

d6ebe4821190fa80d81e662db3a7d224781fbfa2
Jhon Arias <7honarias@gmail.com>
2020-12-17 18:00:09 +0000

    [ADD] - correciton ;

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return (None)
 
-    x = 0;
+    x = 0
     for element in a_dictionary:
         if a_dictionary[element] > x:
             x = a_dictionary[element]

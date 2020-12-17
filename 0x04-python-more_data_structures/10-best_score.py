62096bd704a2014c5af976611dec8227d3119a90
Jhon Arias <7honarias@gmail.com>
2020-12-17 18:43:24 +0000

    [ADD] - Correction with sorted

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return (None)
-
-    x = 0
-    for element in a_dictionary:
-        if a_dictionary[element] > x:
-            x = a_dictionary[element]
-            ids = element
-    return (ids)
+    if a_dictionary is not None:
+        li_values = sorted(a_dictionary.values())
+        for name, value in a_dictionary.items():
+            if value == li_values[-1]:
+                return(name)
+    return (None)

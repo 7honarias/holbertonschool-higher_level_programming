3ce59723f8ff2b7b8f976592079ce258bc229ab4
Jhon Arias <7honarias@gmail.com>
2020-12-17 20:04:22 +0000

    [ADD] - correction isinstance

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
 
-    if type(roman_string) == type(''):
+    if isinstance(roman_string, str):
         dic_roman = {
             "X": 10, "I": 1, "V": 5, "C": 100, "L": 50, "D": 500, "M": 1000}
         result = 0
         temp = 0
-        for str in roman_string:
-            value = dic_roman[str]
+        for sr in roman_string:
+            value = dic_roman[sr]
             if temp >= value or temp == 0:
                 result += value
             else:

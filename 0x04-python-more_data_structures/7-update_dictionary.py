20732adfe4a0bf4ae162b06fed74a2efbe42e3a6
Jhon Arias <7honarias@gmail.com>
2020-12-17 17:15:10 +0000

    [ADD] - update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+
+    if a_dictionary.get(key) is None:
+        a_dictionary.update({key: value})
+        return (a_dictionary)
+
+    a_dictionary[key] = value
+    return (a_dictionary)

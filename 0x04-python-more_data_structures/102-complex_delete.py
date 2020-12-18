4237c226dd49151b39cd37dd67e5925402dcc2a2
Jhon Arias <7honarias@gmail.com>
2020-12-18 11:35:52 +0000

    [ADD] -  complex delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/pyhon3
+def complex_delete(a_dictionary, value):
+    names = list(filter(lambda x: (a_dictionary[x] == value), a_dictionary))
+    for n in names:
+        del a_dictionary[n]
+    return (a_dictionary)

b2cdcd781d0bcb0a70220359058c18cc69e16992
Jhon Arias <7honarias@gmail.com>
2020-12-18 10:43:49 +0000

    [ADD] - correction case list is empty

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     top, inf = 0, 0
+    if my_list:
+        for item in my_list:
+            top += (item[0] * item[1])
+            inf += item[1]
 
-    for item in my_list:
-        top += (item[0] * item[1])
-        inf += item[1]
+        return (top/inf)
 
-    return (top/inf)
+    return (0)

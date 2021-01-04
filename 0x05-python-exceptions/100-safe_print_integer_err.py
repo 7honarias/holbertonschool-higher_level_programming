301e8a9d9b12462d0a250f94cb6bcae061ee40b6
Jhon Arias <7honarias@gmail.com>
2021-01-04 18:03:28 +0000

    [ADD] - files of proyect

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def validation(value):
+	x = value
+	if value < 0:
+		x = value * -1
+	if type(value) != int:
+		raise TypeError
+		
+def safe_print_integer_err(value):
+	try:
+		validation(value)
+		print("{:d}".format(value))
+	except TypeError:
+		return (False)
+	return (True)

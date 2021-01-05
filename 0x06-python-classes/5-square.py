b7fe64d26d61e3709669d4fdc20e0e527f5f60c6
Jhon Arias <7honarias@gmail.com>
2021-01-05 22:47:20 +0000

    [UPDATE] - correction comment

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,12 +3,29 @@
 
 
 class Square:
+    """Class define square"""
+    
     def __init__(self, size=0):
         """ Private instance attribute: size:
         property def size(self): to retrieve it
         property setter def size(self, value): to set it: """
 
         self.size = size
+
+
+    def my_print(self):
+        """ function print with #"""
+
+        if self._Square__size == 0:
+            print()
+        else:
+            for x in range(0, self._Square__size):
+                print("{}".format("#" * self._Square__size))
+
+    def area(self):
+        """ return the area of square """
+
+        return self._Square__size ** 2
 
     @property
     def size(self):
@@ -26,16 +43,4 @@ class Square:
             raise ValueError("size must be >= 0")
         self._Square__size = value
 
-    def my_print(self):
-        """ function print with #"""
-
-        if self._Square__size == 0:
-            print()
-        else:
-            for x in range(0, self._Square__size):
-                print("{}".format("#" * self._Square__size))
-
-    def area(self):
-        """ return the area of square """
-
-        return self._Square__size ** 2
+    

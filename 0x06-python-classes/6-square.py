15d565d9d97f2327d9289a04fdfb469ad49f4e4c
Jhon Arias <7honarias@gmail.com>
2021-01-06 00:59:42 +0000

    [UPDATE] - correction functions

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,62 +1,98 @@
 #!/usr/bin/python3
-""" class Square"""
+"""Define a class Square"""
 
 
 class Square:
-    """ a class square"""
+    """Class that defines a square"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """define class """
-
+        """Method for initialize a square object
+           with validation of the data type of size variable.
+        Args:
+            size(int): Size of the side of the square.
+            position(tuple): Position of the square.
+        """
         self.size = size
         self.position = position
 
+    def area(self):
+        """Method that calculate the area of a square.
+        Args:
+           Any Arguments
+        Returns:
+           The current square area.
+        """
+        return (self.__size) ** 2
+
+    def my_print(self):
+        """Method that prints in stdout the square
+           with the character #
+        Args:
+           Any Arguments
+        Returns:
+           Always nothing.
+        """
+        if self.__size == 0:
+            print()
+        elif self.__position[1] > 0:
+            blank_line = '\n' * self.__position[1]
+            print(blank_line, end='')
+        hash_symbol = '#' * self.__size
+        space = ' ' * self.__position[0]
+        for i in range(self.__size):
+            print("{}{}".format(space, hash_symbol))
+
     @property
     def size(self):
-        """ getter of size"""
-
-        return self._Square__size
+        """Getter of size
+        Args:
+           Any Arguments
+        Returns:
+           The current value of size.
+        """
+        return self.__size
 
     @size.setter
     def size(self, value):
-        """ setter of size"""
-
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise TypeError("size must be >= 0")
-        self._Square__size = value
+        """Setter of size
+        Args:
+           value(int): Size of the side of the square.
+        Return:
+           Always nothing
+        """
+        if isinstance(value, int) is True:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')
 
     @property
     def position(self):
-        """ getter of size"""
-
-        return self._position
+        """Getter of position of the square.
+        Args:
+           Any Arguments.
+        Returns:
+           The current value of th position of the square.
+        """
+        return self.__position
 
     @position.setter
     def position(self, value):
-        """ setter of size"""
-
-        msg = "position must be a tuple of 2 positive integers"
+        """Setter of position of the square.
+        Args:
+           value(tuple): Position of the square.
+        Return:
+           Always nothing
+        """
+        error = 'position must be a tuple of 2 positive integers'
         if type(value) is not tuple or len(value) != 2:
-                raise TypeError(msg)
-        for i in value:
-            if (not isinstance(i, int)) or i < 0:
-                raise TypeError(msg)
-        self._position = value
-
-    def area(self):
-        """ function area"""
-
-        return self._Square__size ** 2
-
-    def my_print(self):
-        """function print"""
-
-        if self._Square__size == 0:
-            print()
-        for i in range(0, self._position[1]):
-            print()
-        for i in range(0, self._Square__size):
-            print("{}".format(" "*self._position[0]), end="")
-            print("{}".format("#"*self._Square__size))
+            raise TypeError(error)
+        else:
+            for i in value:
+                if type(i) is not int or i < 0:
+                    raise TypeError(error)
+                else:
+                    continue
+        self.__position = value

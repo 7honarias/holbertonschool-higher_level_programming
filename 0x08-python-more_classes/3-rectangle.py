34090634ffc0fe745f8eb947ac88f81a0d1b6dab
Jhon Arias <7honarias@gmail.com>
2021-01-11 23:22:45 +0000

    [UPDATE] - correction isinstance

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -12,7 +12,7 @@ Raises:
 
 
 class Rectangle:
-    """def rectangle"""
+    ''' define the rectangle type '''
 
     def __init__(self, width=0, height=0):
         self.width = width
@@ -20,29 +20,29 @@ class Rectangle:
 
     @property
     def width(self):
-        return(self._Rectangle__width)
+        return self.__width
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-
-        self._Rectangle__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
-        return(self._Rectangle__height)
+        return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-
-        self._Rectangle__height = value
+        else:
+            self.__height = value
 
     def area(self):
         re_area = self._Rectangle__width * self._Rectangle__height

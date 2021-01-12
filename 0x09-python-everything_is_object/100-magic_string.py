eb8fc59d7177b6d3896e118ee204170482ecde13
Jhon Arias <7honarias@gmail.com>
2021-01-12 17:42:32 +0000

    [ADD] - project python

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(stri=[]):
+    stri += ["Holberton"]
+    return(', '.join(stri))
---
+++ 0x09-python-everything_is_object/101_locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""class LockedClass"""
+
+
+class LockedClass:
+    def __init__(self):
+        self.first_name 

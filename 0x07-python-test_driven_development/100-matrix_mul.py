a8fd5e8d9d7e6b7fdb14a3abaf3edd4953198c7a
Jhon Arias <7honarias@gmail.com>
2021-01-15 16:49:43 +0000

    [UPDATE] - test in advance

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -17,10 +17,10 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_b can't be empty")
 
     for i in m_a:
-        if not isinstance(i, list):
-            raise TypeError("m_a must be a list of lists")
         if len(i) < 1:
             raise ValueError("m_a can't be empty")
+        if not isinstance(i, list):
+            raise TypeError("m_a must be a list of lists")
         for x in i:
             if type(x) not in (int, float):
                 raise TypeError("m_a should contain only integers or floats")
@@ -30,10 +30,10 @@ def matrix_mul(m_a, m_b):
             raise TypeError("each row of m_a must be of the same size")
 
     for i in m_b:
-        if not isinstance(i, list):
-            raise TypeError("m_a must be a list of lists")
         if len(i) < 1:
             raise ValueError("m_b can't be empty")
+        if not isinstance(i, list):
+            raise TypeError("m_a must be a list of lists")
         for x in i:
             if type(x) not in (int, float):
                 raise TypeError("m_b should contain only integers or floats")
@@ -43,7 +43,7 @@ def matrix_mul(m_a, m_b):
             raise TypeError("each row of m_b must be of the same size")
 
     if a != len(m_b):
-        raise TypeError("m_a and m_b can't be multiplied")
+        raise ValueError("m_a and m_b can't be multiplied")
 
     m = 0
     suma = 0

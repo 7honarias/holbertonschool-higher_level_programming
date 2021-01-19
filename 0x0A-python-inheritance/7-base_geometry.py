dda404d87d693c40f2b2f442b80a8aaa3eeaa8f2
7honarias <7honarias@gmail.com>
2021-01-19 18:13:12 -0500

    [UPDATE] - add comments of functions

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,9 +6,13 @@ class BaseGeometry:
     """ declarations"""
 
     def area(self):
+        """ calculate area"""
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """ validator int"""
+
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
 

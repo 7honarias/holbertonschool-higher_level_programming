51f46ac4f85d8936d18f8483c750ee40f3563233
7honarias <7honarias@gmail.com>
2021-01-19 19:07:57 -0500

    [UPDATE] - correction errors comments

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@
 def inherits_from(obj, a_class):
     """compare function"""
 
-    if type(obj) is not a_class:
+    if type(obj) != a_class and isinstance(obj, a_class):
         return(True)
     else:
         return(False)
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,17 +6,19 @@ class BaseGeometry():
     """ declarations"""
 
     def area(self):
+        """ Raises Exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """validates value"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
+    """class Rectangle"""
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self._Rectangle__width = width

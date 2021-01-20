1d258de6d5304a2f722397d82fcce9f8d8ffc26d
7honarias <7honarias@gmail.com>
2021-01-20 18:16:41 -0500

    [UPDATE] - correction comment 2 -3

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,7 @@
 
 
 def append_write(filename="", text=""):
+    """append of file"""
     with open(filename, mode="a", encoding="utf-8") as f:
         total = f.write(text)
 
--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ to json string"""
-import os
 import json
 
 
 def to_json_string(my_obj):
+    """function to json string"""
     my_obj = json.dumps(my_obj)
 
     return my_obj

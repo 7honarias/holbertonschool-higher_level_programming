3770cd37c4edc0244c78af43a84ea84efbbf6ff8
7honarias <7honarias@gmail.com>
2021-01-19 20:26:03 -0500

    [ADD] - exercise scuare #1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,37 +2,13 @@
 """ class Rectangle"""
 
 
-class BaseGeometry():
-    """ declarations"""
-
-    def area(self):
-        return(self._Rectangle__width * self._Rectangle__height)
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self._Rectangle__width = width
-        self.integer_validator("height", height)
-        self._Rectangle__height = height
-
-        self.area()
-
-    def __str__(self):
-        return ("[Rectangle] {}/{}".format(self._Rectangle__width, self._Rectangle__height))
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    def__init__(self, size):
+    def __init__(self, size):
         self.integer_validator("size", size)
         self._Square__size = size
 
-        
-
+    def area(self):
+        return self._Square__size ** 2        

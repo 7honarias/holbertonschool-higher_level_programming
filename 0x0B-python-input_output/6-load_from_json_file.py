f954a7759db2e6995c12a05aa63a68b9ef151995
7honarias <7honarias@gmail.com>
2021-01-20 18:24:55 -0500

    [UPDATE] - correction comment 7-6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """create object from a json file"""
-import os
 import json
 
 
 def load_from_json_file(filename):
+    """filename is file to read"""
     with open(filename, mode="r", encoding="UTF8") as f:
         obj = f.read()
         m_list = json.loads(obj)

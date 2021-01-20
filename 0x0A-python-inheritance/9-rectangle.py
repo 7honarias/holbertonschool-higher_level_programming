319b2123f6280c26943fe2ade2be809256010a39
7honarias <7honarias@gmail.com>
2021-01-19 19:50:00 -0500

    [ADD] - area in metod

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,15 +4,18 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """class rectangle"""
     def __init__(self, width, height):
+        """inistanciation of atributes"""
         self.integer_validator("width", width)
         self._Rectangle__width = width
         self.integer_validator("height", height)
         self._Rectangle__height = height
 
-        self.area()
+    def area(self):
+        return self._Rectangle__width * self._Rectangle__height
 
     def __str__(self):
         """return string"""

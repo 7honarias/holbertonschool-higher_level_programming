5c616830365df730282695361ca0e4cd7b74070c
7honarias <7honarias@gmail.com>
2021-01-24 12:44:10 -0500

    [update] all proyecst in wsl

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import json
+import csv
 """ class base"""
 
 
@@ -20,11 +21,12 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        if len(list_objs) <= 1:
-            return ([])
-        new_list = []
-        for i in list_objs:
-            new_list += [i.to_dictionary()]
+        if len(list_objs) <= 1 or list_objs is None:
+            new_list = []
+        else:
+            new_list = []
+            for i in list_objs:
+                new_list += [i.to_dictionary()]
         my_f = cls.__name__ + ".json"
         with open(my_f, mode='w', encoding='utf-8') as f:
             f.write(cls.to_json_string(new_list))
@@ -36,4 +38,69 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
-        s = cls
+        """create a new instance"""
+        from models.rectangle import Rectangle
+        from models.square import Square
+
+        if cls is Rectangle:
+            dummy = cls(1, 1)
+        elif cls is Square:
+            dummy = cls(1)
+        else:
+            dummy = None
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """load form file Rectangle.json Square.json"""
+        import os
+        my_list = list()
+        my_file = cls.__name__ + ".json"
+        if os.path.isfile(my_file):
+            with open(my_file, mode="r", encoding='utf-8') as f:
+                text = f.read()
+                list_output = cls.from_json_string(text)
+                for diction in list_output:
+                    dum = cls.create(**diction)
+                    my_list.append(dum)
+                return (my_list)
+        return(my_list)
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        
+        filename = cls.__name__ + ".csv"
+        list_dictionary = list(map(cls.to_dictionary, list_objs))
+        keys = list()
+        for key in list_dictionary[0]:
+            keys.append(key)
+            
+        with open(filename, mode="w") as f:
+            dict_writer = csv.DictWriter(f, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(list_dictionary)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ load_from_file_csv """
+        filename = '{}.csv'.format(cls.__name__)
+        try:
+            with open(filename, mode='r', newline='') as file:
+                if cls.__name__ is 'Square':
+                    obj_field = ["id", "size", "x", "y"]
+                else:
+                    obj_field = ["id", "width", "height", "x", "y"]
+                dic_list = csv.DictReader(file)#fieldnames=obj_field
+                my_list = []
+                for dicts in dic_list:
+                    dictionary = {}
+                    for key in dicts:
+                        dictionary[key] = int(dicts[key])
+                    my_list.append(dictionary)
+                return [cls.create(**dicts) for dicts in my_list]
+        except IOError:
+            return []
+
+    def draw(list_rectangles, list_squares):
+        

4245c35b1811949bd3027fb75e2ec05d5dc1acdd
7honarias <7honarias@gmail.com>
2021-01-26 12:42:55 -0500

    [ADD] - test unittest

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,27 +1,44 @@
+#!/usr/bin/python3
+""" This is a Unittest module """
+
+
 import unittest
-from models.rectangle import Rectangle
+from models.base import Base
 
 
 class TestRectangle(unittest.TestCase):
+    """ Test Cases for Base Class """
 
-    def test_width(self):
-        r = Rectangle(2, 3)
-        self.assertAlmostEqual(r.width, 2)
-        self.assertAlmostEqual(r.height, 3)
-        self.assertAlmostEqual(type(r), Rectangle)
-        self.assertAlmostEqual(r.area(), 6)
-
-    def test_rectangle_init(self):
-        r1 = Rectangle(2, 3)
-        self.assertAlmostEqual(r1.id, 2)
-        b2 = Rectangle(2, 4)
-        self.assertAlmostEqual(b2.id, 3)
-
-    def test_raise(self):
-        r3 = Rectangle(2, 3)
-        assertIsInstance(r3, Rectangle, msg=None)
-
+    def setUp(self):
+        """This method set up initial state for all test methods"""
+        Base._Base__nb_objects = 0
+        # print("setUp")
 
     def tearDown(self):
-        Base._Base__nb_objects = 0
+        """This method to perform cleanup after each test method completes"""
+        # print("tearDown")
 
+    def test_id_single(self):
+        """ Test for set id function """
+        b0 = Base(1)
+        self.assertEqual(b0.id, 1)
+
+    def test_id_none(self):
+        """ Test for set id function """
+        b0 = Base(None)
+        self.assertEqual(b0.id, 1)
+
+    def test_id_multiple(self):
+        """ Test for set id function """
+        b0 = Base()
+        b1 = Base()
+        self.assertEqual(b0.id, 1)
+        self.assertEqual(b1.id, 2)
+
+    def test_id_error(self):
+        """ Test for set id function """
+        self.assertEqual("Juan", Base("Juan").id)
+        self.assertEqual(2.5, Base(2.5).id)
+        self.assertEqual([1, 2], Base([1, 2]).id)
+        self.assertEqual({'1': 2}, Base({'1': 2}).id)
+        self.assertEqual(True, Base(True).id)

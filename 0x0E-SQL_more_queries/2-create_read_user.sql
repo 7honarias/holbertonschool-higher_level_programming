e6523e895cbb9fefb53d17e8122c29812a5496fd
7honarias <7honarias@gmail.com>
2021-03-02 22:44:35 +0000

    [ADD] - exercise of 1 to 6

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- script taht create the database hbtn_0d_2 and user user_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' identified by 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';

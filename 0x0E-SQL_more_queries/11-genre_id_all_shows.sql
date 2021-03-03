ec776df97734db4a9259fa5c2987357b3e9e8e4c
7honarias <7honarias@gmail.com>
2021-03-03 14:53:20 +0000

    [ADD] - correction of exercise 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- script that lists all shows contained in the 
-SELECT shows.title, tv_show_genres.genre_id
+SELECT shows.title, genres.genre_id
 FROM tv_shows AS shows
 LEFT JOIN tv_show_genres AS genres
-ON genres.genre_id = shows.id
+ON genres.show_id = shows.id
 ORDER BY shows.title ASC, genres.genre_id ASC;

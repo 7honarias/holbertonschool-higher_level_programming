be7caa2555a5fc05cac77582e708e697a7c0b253
7honarias <7honarias@gmail.com>
2021-03-03 15:38:47 +0000

    [ADD] - exercise 13 sql genres

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- script that lists all shows contained
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_show_genres.genre_id=tv_shows.id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC;
+SELECT shows.title, show_genres.genre_id
+FROM tv_shows AS shows
+LEFT JOIN tv_show_genres AS show_genres
+     ON show_genres.genre_id = shows.id
+WHERE show_genres.genre_id IS NULL
+ORDER BY shows.title ASC;

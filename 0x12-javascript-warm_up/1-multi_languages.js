d95eb69a3b6c0e5bc2ae7291b5c9309c3867fabd
7honarias <7honarias@gmail.com>
2021-03-22 12:34:10 -0500

    [UPDATE] - correction quotes

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let c = "C is fun\n";
-let python = "Python is cool\n";
-let javaScript = "JavaScript is amazing"
+let c = 'C is fun';
+let python = 'Python is cool';
+let javaScript = 'JavaScript is amazing'
 
 console.log(c);
 console.log(python);

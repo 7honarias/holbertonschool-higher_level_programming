7b51fd1ce8239e567ceaa00ca9ba839689fe1353
7honarias <7honarias@gmail.com>
2021-03-22 18:35:02 +0000

    [UPDATE] - error semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 process.argv.forEach((val, index) => {
-    if (index > 1){
-        console.log(val);
-    }
+  if (index > 1) {
+    console.log(val);
+  }
 });
-if (process.argv.length > 2){
-    console.log("Argument found");
-}else{
-    console.log("No argument")
+if (process.argv.length > 2) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
 }

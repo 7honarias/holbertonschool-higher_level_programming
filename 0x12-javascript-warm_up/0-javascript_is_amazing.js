276c30fe4e8a12cfd35dc95b495bb8e989f1c04b
7honarias <7honarias@gmail.com>
2021-03-22 12:36:37 -0500

    [UPDATE] - comments

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-
-// constant variable with const.
+/* constant variable with const.*/
 const myVar = 'Javascript is amazing';
-
-// print content with console.log()
 console.log(myVar);

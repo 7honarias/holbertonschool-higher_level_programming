7f7ea98344436c7b1965ac6e483aab792b85fcb8
7honarias <7honarias@gmail.com>
2021-03-23 19:10:17 +0000

    [ADD] exercises until 9 logme

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const A = require('./5-square');
+
+class Square extends A {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+module.exports = Square;

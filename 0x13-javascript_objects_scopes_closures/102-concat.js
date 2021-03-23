46e88f4e36762188868f52225f68d4f05f2adece
7honarias <7honarias@gmail.com>
2021-03-23 23:00:51 +0000

    [UPDATE] - correction create file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -8,10 +8,20 @@ fs.readFile(arrArg[0], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   }
-  ff.readFile(arrArg[1], 'utf8', function (err2, data2) {
+  fs.appendFile(arrArg[2], data, function (err2) {
     if (err2) {
       console.log(err2);
     }
-    console.log(data + data2);
+  });
+});
+
+ff.readFile(arrArg[1], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  }
+  fs.appendFile(arrArg[2], data, function (err2) {
+    if (err2) {
+      console.log(err2);
+    }
   });
 });

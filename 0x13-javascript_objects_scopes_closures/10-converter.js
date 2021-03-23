24afd13a3e2696f9eb0a6c3f5b308614ce4ea4b8
7honarias <7honarias@gmail.com>
2021-03-23 22:53:42 +0000

    [ADD] - exercise 13 advanced

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  function convert(number) {
+  function convert (number) {
     return number.toString(base);
   }
   return convert;
-}
+};
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
 const arrArg = process.argv.slice(2);
-let str = '';
-fs = require('fs');
 
-fs.readFile(arrArg[0], 'utf8', function(err, data){
-  return data;
+const ff = require('fs');
+const fs = require('fs');
+
+fs.readFile(arrArg[0], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  }
+  ff.readFile(arrArg[1], 'utf8', function (err2, data2) {
+    if (err2) {
+      console.log(err2);
+    }
+    console.log(data + data2);
+  });
 });
-
-console.log(str);

396beee6572d1b1b6473ab18f1728c5dbb3ef0b1
7honarias <7honarias@gmail.com>
2021-03-27 15:30:32 +0000

    [ADD] - correction error main

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
     query_rows = cur.fetchall()
 
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
+# List that start in N
 
 import MySQLdb
+
 from sys import argv
 
-username = argv[1]
-password = argv[2]
-database = argv[3]
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
 
-conn = MySQLdb.connect(host="localhost", port=3306,  user=username,
-                       passwd=password,
-                       db=database, charset="utf8")
-cur = conn.cursor()
+    conn = MySQLdb.connect(host="localhost", port=3306,  user=username,
+                           passwd=password,
+                           db=database, charset="utf8")
+    cur = conn.cursor()
 
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-query_row = cur.fetchall()
+    query_row = cur.fetchall()
 
-for row in query_row:
-    print(row)
+    for row in query_row:
+        print(row)
 
-cur.close()
-conn.close()
+    cur.close()
+    conn.close()

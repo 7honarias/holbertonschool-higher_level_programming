6e8eb6e571f6f2e89fab0cec8297db1f348c8ef0
7honarias <7honarias@gmail.com>
2021-05-04 18:19:18 +0000

    [ADD] . completed task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,18 +3,24 @@ const request = require('request');
 
 const mUrl = process.argv[2];
 let userId;
-let dictUserTaskComplete
-request.get(mUrl, function(err, response, body){
-    if (err){
-        console.log(err.message);
+const dictUserTaskComplete = {};
+request.get(mUrl, function (err, response, body) {
+  if (err) {
+    console.log(err.message);
+  }
+  const todos = JSON.parse(body);
+  for (const task in todos) {
+    userId = todos[task].userId;
+    if (dictUserTaskComplete.hasOwnProperty(userId)) {
+      if (todos[task].completed === true) {
+        let count = dictUserTaskComplete[userId];
+        count += 1;
+        console.log(count);
+        dictUserTaskComplete[userId] = count;
+      }
+    } else {
+      dictUserTaskComplete[userId] = 1;
     }
-    todos = JSON.parse(body);
-    for (task in todos) {
-        userId = todos[task]['userId'];
-        if( dictUserTaskComplete.hasOwnProperty(userId) ) {
-            let count = dictUserTaskComplete[userId];
-            count += 1;
-        }
-        console.log(todos[task]['id'])
-    }
+  }
+  console.log(dictUserTaskComplete);
 });

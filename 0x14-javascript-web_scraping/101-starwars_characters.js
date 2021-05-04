40a8673b56daea73e994af38fca4c7a60bd9d50a
7honarias <7honarias@gmail.com>
2021-05-04 23:41:45 +0000

    [ADD] - task starwars_characters

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+
+const filmId = process.argv[2];
+const mUrl = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+request.get(mUrl, function (err, response, body) {
+  if (err) {
+    console.log(err.message);
+  }
+  const filmCharacters = JSON.parse(body).characters;
+  filmCharacters.forEach(characters => {
+    request.get(characters, function (err, response, body) {
+      if (err) {
+        console.log(err.message);
+      }
+      const result = JSON.parse(body);
+      console.log(result.name);
+    });
+  });
+});

1ac71066ff143eb4333da8dbc2beae140e4c83e3
7honarias <7honarias@gmail.com>
2021-05-12 18:18:15 +0000

    [ADD] - javascript web jQuery

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,9 @@
+url = 'https://fourtonfish.com/hellosalut/?lang=';
+$(document).ready(function () {
+  $('INPUT#btn_translate').on('click', function () {
+    const lan = $('INPUT#language_code').val();
+    $.getJSON(url + lan, function (data) {
+      $('DIV#hello').text(data.hello);
+    });
+  });
+});
